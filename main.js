7b5f2a5752272cea6b4317a0bb20ef992a66421d
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-04-22 11:00:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//  Q no 100
+var sruareRoot = Math.sqrt(144);
+console.log(sruareRoot);
+//  Q no 101
+function getRandomInt() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(getRandomInt());
+//  Q no 102
+var difference = Math.abs(-5 - 5);
+console.log(difference);
---
+++ main.ts
@@ -0,0 +1,17 @@
+//  Q no 100
+
+const sruareRoot =Math.sqrt(144);
+console.log(sruareRoot);
+
+
+//  Q no 101
+
+function getRandomInt():number{
+    return Math.floor(Math.random() * 10) + 1
+}
+console.log(getRandomInt());
+
+//  Q no 102
+
+const difference = Math.abs(-5 - 5);
+console.log(difference);
